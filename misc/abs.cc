b4d58b856d6befe19fe2dc520316e3650684800f
Kevin Gao <kevin.gao@duke.edu>
2012-02-15 03:26:25 -0500

    Added misc/abs.cc which computes an absolute value without the use of logical operators

---
+++ misc/abs.cc
@@ -0,0 +1,34 @@
+/* Kevin Gao
+ * Takes the absolute value without logic statements
+ */
+
+#include <iostream>
+#include <sstream>
+using namespace std; 
+
+#define INTWIDTH sizeof(int)*8-1
+int myAbs(int i);
+
+int main(int argc, const char* argv[]) {
+	if (argc > 2) return -1;
+
+	int i;
+	if (argc == 2) {
+		stringstream(argv[1]) >> i;
+	} else {
+		i = -1;
+	}
+	cout << myAbs(i) << endl;
+	return 0;
+}
+
+int myAbs(int i) {
+	// Bit-wise AND 0x100... with number,
+	// then arithmetic shift right to get
+	// either 0x111... or 0x000...
+	int m = ((1<<INTWIDTH) & i) >> INTWIDTH;
+	// XOR m with number to flip bits if
+	// 0x111... was the result. Finally,
+	// AND m with 0x1 and add.
+	return (m^i) + (m&1);
+}
